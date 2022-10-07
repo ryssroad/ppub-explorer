a5bb0baec4bf517715282fd7ec9651a79dc2412a
ryssroad <ryssroad@gmail.com>
2022-10-07 11:28:57 +0600

    footer

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,30 +1,13 @@
 <template>
   <p class="clearfix mb-0">
-    <span class="float-md-left d-none d-md-block d-md-inline-block mt-25">
-      Powered By
+    <span class="float-md-center d-none d-md-block d-md-inline-block mt-25">
+      modified by
       <b-link
         class="ml-25 font-weight-bolder"
         href="#"
         target="_blank"
-      >Ping.pub</b-link>
+      >ryssroad#7010</b-link>
     </span>
-
-    <router-link
-      v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-      class="float-md-right"
-      to="/coffee"
-    >Buy me a cup of coffee.
-      <span
-        v-ripple.400="'rgba(113, 102, 240, 0.15)'"
-        variant="outline-primary"
-      >
-        <feather-icon
-          icon="HeartIcon"
-          size="21"
-          class="text-danger stroke-current"
-        />
-      </span>
-    </router-link>
   </p>
 </template>
 
